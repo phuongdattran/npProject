ea5c0db7154d47d2f0e6707c549fb9a0e2cf957a
phuongdattran <tran.phuongdat@gmail.com>
2020-10-13 12:35:45 +0200

    Update template

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,3 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+var el = document.querySelector('.tabs');
+var instance = M.Tabs.init(el, {});
